8e00159df5723750c0e1a69e98e69b1f85689059
Nando717 <iverlandioedsonn@gmail.com>
2026-07-09 21:18:10 -0300

    funções em arquivo funcoes, sistema atualizado

--- calculadora.py
+++ calculadora.py
@@ -11,30 +11,17 @@
 #organizando o sistema para mudanças
 #criando menu para calculadora
 
+from funcoes import adicao,subtracao,multiplicacao,divisa
+
+
+
+
+
+
+
+
 operacao = int(input('digite o numero da operaçao desejada: 1 adicao, 2 subtracao, 3 multipicacao, 4 divisao: '))
 
-
-
-def adicao():
- num1 = float(input('digite o primeiro número: '))
- num2 = float(input('digite o segundo numero: '))
- return num1 + num2
-
-
-def subtracao():
- num1 = float(input('digite seu primeiro número: '))
- num2 = float(input('digite seu segundo numero: ')) 
- return num1 - num2
-
-def multiplicacao():
-  num1 = float(input('digite seu primeiro número: '))
-  num2 = float(input('digite seu segundo número: '))
-  return num1 * num2
-
-def divisao():
-  num1 = float(input('digite seu primeiro número: '))
-  num2 = float(input('digite seu segundo número: '))
-  return num1 / num2
 
 if operacao == 1:
   print(adicao())
